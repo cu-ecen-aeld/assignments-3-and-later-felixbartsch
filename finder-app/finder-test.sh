d53f21b2f39c4090d04b8a1cfe2588075cd93e77
felixbartsch <felix.bartsch@yahoo.de>
2025-09-28 20:07:16 +0300

    Make finder-test executable from other directories

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,6 +5,7 @@
 set -e
 set -u
 
+RUNDIR=$(dirname $0)
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
@@ -30,7 +31,7 @@ MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines a
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
-
+cd $RUNDIR
 # create $WRITEDIR if not assignment1
 assignment=`cat conf/assignment.txt`
 
